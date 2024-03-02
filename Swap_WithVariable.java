543eff0c57982448c1b1b257e3b88b12384ae6b9
Swetha Dornala <dornalaswetha1@gmail.com>
2024-03-03 00:35:24 +0530

    secondcommit

---
+++ Swap_WithVariable.java
@@ -0,0 +1,18 @@
+package AssignmentsOfJava;
+
+public class Swap_WithVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=40;
+		int c;
+		c=b;
+		b=a;
+		a=c;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
