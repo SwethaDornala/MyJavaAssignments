a96db39753bf5df8bd09d09f12fff72d6748d987
Swetha Dornala <dornalaswetha1@gmail.com>
2024-03-04 19:35:12 +0530

    Andfile

---
+++ ANDOperator.java
@@ -0,0 +1,19 @@
+//Assignment : Write a program for AND operator
+package AssignmentsOfJava;
+
+public class ANDOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=0;
+		int b=1;
+		
+		if(b>a && b==2)
+			System.out.println("If condition is true");
+		else
+			System.out.println("Else condition");
+
+	}
+
+}
