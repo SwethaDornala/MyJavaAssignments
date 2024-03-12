bcc04a3b97943d0a9b93032bb47a65095fdfb1bc
Swetha Dornala <dornalaswetha1@gmail.com>
2024-03-12 23:29:20 +0530

    UpcastToObjectClass

---
+++ Swetha.java
@@ -0,0 +1,17 @@
+package TypeCasting;
+
+public class Swetha {
+	void add()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Object o=new Swetha();
+		System.out.println(o.getClass());
+
+	}
+
+}
