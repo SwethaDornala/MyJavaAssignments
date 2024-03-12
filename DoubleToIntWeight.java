aa387316abd93f17db548feafa449249a2716e27
Swetha Dornala <dornalaswetha1@gmail.com>
2024-03-12 23:35:54 +0530

    WeightdataconversionBETWEENInt and Double

---
+++ DoubleToIntWeight.java
@@ -0,0 +1,13 @@
+package TypeCasting;
+
+public class DoubleToIntWeight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double weight=51.4;
+		int weigh=(int)weight;
+		System.out.println(weigh);
+
+	}
+
+}
---
+++ IntToDoubleWeight.java
@@ -0,0 +1,14 @@
+package TypeCasting;
+
+public class IntToDoubleWeight {
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int weight=52;
+			double weigh=weight;
+			System.out.println(weigh);
+
+		}
+
+	}
