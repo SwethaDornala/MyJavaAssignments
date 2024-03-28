490ddd26b62d0979f277d8a25d14c289fcf229db
Swetha Dornala <dornalaswetha1@gmail.com>
2024-03-29 00:14:20 +0530

    CSS selector assignments

---
+++ SeleniumAssignments/CSSSelectorByID.java
@@ -0,0 +1,21 @@
+package CSSSelectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CSSSelectorByID {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+		WebElement searchtextfield=driver.findElement(By.cssSelector("#APjFqb"));
+		searchtextfield.sendKeys("India");
+		searchtextfield.sendKeys(Keys.ENTER);
+		driver.close();
+	}
+
+}
